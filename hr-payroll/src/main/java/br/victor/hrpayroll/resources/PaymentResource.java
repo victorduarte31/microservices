379206601cdfd131baf940c8dc78517d02f111cb
Victor Duarte <devvictorduarte@gmail.com>
2021-11-23 14:29:38 -0300

    create app hr-api-gateway

--- hr-payroll/src/main/java/br/victor/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/victor/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.victor.hrpayroll.resources;
 
 import br.victor.hrpayroll.entities.Payment;
 import br.victor.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,20 @@ public class PaymentResource {
 
     private final PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+
+        return ResponseEntity.ok(Payment.builder()
+                .name("Victor")
+                .dailyIncome(500.0)
+                .days(days).build());
+
+    }
+
 }
